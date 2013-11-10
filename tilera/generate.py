e5670021eeb569957ce7df01d868a6ecb79905e8
Minke Zhang <minke.zhang@gmail.com>
2013-11-10 17:52:42 -0500

    added generation scripts

---
+++ tilera/generate.py
@@ -0,0 +1,44 @@
+from random import random
+from math import ceil
+from os import path, mkdir
+
+def main(folder, size, noise):
+	if(not path.exists(folder)):
+		mkdir(folder)
+
+	bound = 100
+	a = []
+	# generate a
+	for i in xrange(size):
+		tmp_row = []
+		for j in xrange(size):
+			if((i == j) or noise > random()):
+				tmp_row.append(rand_int(bound))
+			else:
+				tmp_row.append(0)
+		a.append(tmp_row)
+	x = []
+	# generate x
+	for i in xrange(size):
+		x.append(rand_int(bound))
+
+	b = []
+	for i in xrange(size):
+		b.append(sum([ (a[i][k] * x[k]) for k in xrange(size) ]));
+
+	fp_a = open(folder + "/a.txt", "w+")
+	fp_b = open(folder + "/b.txt", "w+")
+	fp_x = open(folder + "/x_in.txt", "w+")
+
+	for i in xrange(size):
+		fp_a.write(" ".join([ str(m) for m in a[i] ]) + "\n")
+		fp_x.write(str(x[i]) + "\n")
+		fp_b.write(str(b[i]) + "\n")
+
+def rand_int(bound):
+	return(ceil(random() * bound) + 1)
+
+for i in xrange(10):
+	noise = i / 10.
+	main("pipe/sparse128_" + str(noise), 128, noise)
+
---
+++ tilera/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+EXECUTABLE=jacobi
+PIPE=pipe
+OUT=results.txt
+FOLDER=sample
+
+rm $OUT
+
+for CORES in 1 2 4 8 16 32 64
+do
+	echo running $CORES
+	date
+	{ time tile-monitor --batch-mode --image tile64 --upload $EXECUTABLE $EXECUTABLE --upload $PIPE $PIPE --run - $EXECUTABLE $FOLDER $CORES - --download $PIPE $PIPE --download $PIPE $PIPE ; } >> $OUT 2>&1
+done
