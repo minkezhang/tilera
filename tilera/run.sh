187c4f261a879f33d8ac1fa3d0fed30015e64216
Minke Zhang <minke.zhang@gmail.com>
2013-11-11 14:23:56 -0500

    matrix generation bug fixed

--- tilera/run.sh
+++ tilera/run.sh
@@ -8,23 +8,23 @@ EMAIL=minke.zhang@gmail.com
 
 for DIM in 1 2 4 8 16 32 64 128 256
 do
-	for CORES in 1 2 4 8 16 32 64
+	for NOISE in 0.1 0.9
 	do
-		if [[ CORES -le DIM ]]
-		then
-			for NOISE in 0.1 0.9
-			do
-				FOLDER=$HEADER$DIM\_$NOISE
+		FOLDER=$HEADER$DIM\_$NOISE
+		python generate.py $DIM $NOISE $HEADER
+		for CORES in 1 2 4 8 16 32 64
+		do
+			if [[ CORES -le DIM ]]
+			then
 				OUT=$RESULTS/$DIM\_$CORES\_$NOISE.txt
 				rm -rf $OUT
 				echo "running n = $DIM on $CORES cores with noise = $NOISE -- $FOLDER"
 				date
 				echo ""
-				python generate.py $DIM $NOISE $HEADER
 				{ time tile-monitor --batch-mode --image tile64 --upload $EXECUTABLE $EXECUTABLE --upload $PIPE/$FOLDER $PIPE/$FOLDER --run - $EXECUTABLE $FOLDER $CORES - --download $PIPE/$FOLDER $PIPE/$FOLDER ; } >> $OUT 2>&1
 			done
-			echo "finished n = $DIM on $CORES cores" | mail -s "simulation update ($DIM | $CORES)" $EMAIL
 		fi
 	done
+	echo "finished n = $DIM" | mail -s "simulation update ($DIM complete)" $EMAIL
 done
 echo "simulations complete"
