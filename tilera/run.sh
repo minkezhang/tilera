fc283fa1819c53017ec18d2edbb4f9bb596e93dd
Minke Zhang <minke.zhang@gmail.com>
2013-12-05 16:46:41 -0500

    documentation edits

--- tilera/run.sh
+++ tilera/run.sh
@@ -11,6 +11,7 @@ do
 	for NOISE in 0.1 0.9
 	do
 		FOLDER=$HEADER$DIM\_$NOISE
+		echo "python generate.py $DIM $NOISE $HEADER"
 		python generate.py $DIM $NOISE $HEADER
 		for CORES in 1 2 4 8 16 32 64
 		do
@@ -19,13 +20,14 @@ do
 				for MODE in 0 1
 				do
 					case "$MODE" in
-					 0) MODESTR=mpi ;;
-					 1) MODESTR=dma ;;
+						0) MODESTR=mpi ;;
+						1) MODESTR=dma ;;
 					esac
 
 					OUT=$RESULTS/$DIM\_$CORES\_$MODESTR\_$NOISE.txt
 					rm -rf $OUT
 					echo "running n = $DIM on $CORES cores with noise = $NOISE and mode = $MODE -- $FOLDER, $OUT"
+					echo "{ time tile-monitor --batch-mode --image tile64 --upload $EXECUTABLE $EXECUTABLE --upload $PIPE/$FOLDER $PIPE/$FOLDER --run - $EXECUTABLE $FOLDER $CORES $MODE - --download $PIPE/$FOLDER $PIPE/$FOLDER ; } >> $OUT 2>&1"
 					date
 					echo ""
 					{ time tile-monitor --batch-mode --image tile64 --upload $EXECUTABLE $EXECUTABLE --upload $PIPE/$FOLDER $PIPE/$FOLDER --run - $EXECUTABLE $FOLDER $CORES $MODE - --download $PIPE/$FOLDER $PIPE/$FOLDER ; } >> $OUT 2>&1
